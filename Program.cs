b755c62413872433a011a6acf385506993f78256
thunderFlydge <thunderFlydge@yandex.ru>
2018-01-24 00:27:31 +0300

    Develop a small (5-6 entities) model for animal world using classes and interfaces. Take List of Animals as data source and perform queries of different types on that data source.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -23,13 +24,18 @@ namespace EncapsulationTask
             Bear bear1 = new Bear(wordList, foodList,"faster",54,"white");
             Bear bear2 = new Bear(wordList, foodList,"slowly",36,"black");
             Bear bear3 = new Bear(wordList, foodList,"medium",45,"brown");
+            Bear bear4 = new Bear(wordList, foodList,"swim",10,"brown");
+            Bear bear6 = new Bear(wordList, foodList,"swim",10,"brown");
+            Bear bear5 = new Bear(wordList, foodList,"medium",45,"white");
             
             Group<Bear> bearsGroup = new Group<Bear>();
             bearsGroup.GroupList.Add(bear1);
-            bearsGroup.GroupList.Add(bear3);
             bearsGroup.GroupList.Add(bear2);
-
-            Console.WriteLine(bearsGroup);
+            bearsGroup.GroupList.Add(bear3);
+            bearsGroup.GroupList.Add(bear4);
+            bearsGroup.GroupList.Add(bear5);
+            bearsGroup.GroupList.Add(bear6);
+            // Console.WriteLine(bearsGroup);
             
             //fishes
             Fish fish1 = new Fish(wordList,foodList,"swim",30, new List<string>()
@@ -38,9 +44,44 @@ namespace EncapsulationTask
                 {"the western Atlantic Ocean"});
             
             Group<Fish> fishGroup = new Group<Fish>(fish1,fish2);
-            Console.WriteLine(fishGroup);
+            //Console.WriteLine(fishGroup);
+            
+            //Data access task
+           
+            var whiteBears = bearsGroup.GroupList
+                .Where(bear => bear.Color == "white")
+                .OrderBy(bear=>bear.MovementSpeed);
+            foreach (var whiteBear in whiteBears)
+            {
+                Console.WriteLine(whiteBear);
+            }
+
+            var notWhiteFastestBear = bearsGroup.GroupList
+                .Where(bear => bear.Color != "white")
+                .OrderBy(bear => -bear.MovementSpeed)
+                .Select(bear => bear).First();
+
+            Console.WriteLine("Not white fastest bear:\n"+notWhiteFastestBear);
+
+
+            var top2SlowestBears = bearsGroup.GroupList
+                .OrderBy(bear => bear.MovementSpeed)
+                .Take(2);
+
+            Console.Write("Top 2 slowest bears:\n" + String.Join("......\n",top2SlowestBears));
+
+
+            var brownBearsMoveTypes =
+                from bear in bearsGroup.GroupList
+                where bear.Color == "brown"
+                select bear.MovementType.ToUpper();
+
+            Console.WriteLine("Types of movement of brown bears without duplicates: " 
+                              + String.Join(", ",brownBearsMoveTypes.Union(brownBearsMoveTypes)));
             
             
+            
+
         }
     }
 }
